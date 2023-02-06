745f42a52b6ddc4565c3fc48c672a67592220a69
syndelldev <106510484+syndelldev@users.noreply.github.com>
2023-02-06 17:19:44 +0530

    dashboard changes

---
+++ src/views/widgets/WidgetBottom.js
@@ -0,0 +1,84 @@
+/* eslint-disable */
+import React from 'react'
+import './widget.css'
+import { CChart } from '@coreui/react-chartjs'
+import {
+    CTableRow,
+    CCard,
+    CTableDataCell,
+    CTableBody,
+    CCardBody,
+    CCardHeader,
+    CTable,
+    CBadge,
+    CCardTitle
+  } from '@coreui/react'
+  const style = {
+    body:{
+        display:'flex'
+    },
+    cardOne:{
+        width:'700px',
+        margin:'10px'
+    },
+    cardTwo:{
+        width:'550px',
+        height:'300px',
+        margin:'10px'
+    }
+
+}
+
+
+const WidgetBottom = () => {
+  return (
+    <div style={style.body}>
+    <CCard style={style.cardOne}>
+    <CCardTitle style={{padding:'10px'}}>    Vehicle Status</CCardTitle>
+       
+        <CCardBody>
+        <CTable>
+            <CTableBody>
+            <CTableRow>
+                <CTableDataCell>Active</CTableDataCell>
+                <CTableDataCell><CBadge color="success" shape="rounded-pill">3</CBadge></CTableDataCell>
+            </CTableRow>
+            <CTableRow>
+                <CTableDataCell>Inactive</CTableDataCell>
+                <CTableDataCell><CBadge color="info" shape="rounded-pill">1</CBadge></CTableDataCell>
+            </CTableRow>
+            <CTableRow>
+                <CTableDataCell>In Shop</CTableDataCell>
+                <CTableDataCell><CBadge color="warning" shape="rounded-pill">1</CBadge></CTableDataCell>
+            </CTableRow>
+            <CTableRow>
+                <CTableDataCell>Out of Service</CTableDataCell>
+                <CTableDataCell><CBadge color="danger" shape="rounded-pill">1</CBadge></CTableDataCell>
+            </CTableRow>
+            </CTableBody>
+            </CTable>
+        </CCardBody>
+    </CCard>
+    <CCard style={style.cardTwo}>
+      <CCardTitle style={{padding:'10px'}}>Average Time / service  :  7:46</CCardTitle>
+      <CCardBody>
+            <CChart style={{height:'500px'}}
+        type="doughnut"
+        data={{
+            labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
+            datasets: [
+            {
+                backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
+                data: [40, 20, 80, 10],
+            },
+            ],
+        }}
+        />
+      </CCardBody>
+    </CCard>
+
+    </div>
+  )
+}
+
+export default WidgetBottom
---
+++ src/views/widgets/widgetDropdownChart.js
@@ -0,0 +1,106 @@
+/* eslint-disable */
+import React from 'react'
+import './style.css'
+import { CChartLine } from '@coreui/react-chartjs'
+import {
+    CTableHead,
+    CTableHeaderCell,
+    CTableRow,
+    CCard,
+    CCardTitle,
+    CTableDataCell,
+    CTableBody,
+    CCardBody,
+    CCardFooter,
+    CCardHeader,
+    CTable,
+    CBadge
+  } from '@coreui/react'
+
+  const style = {
+    cardOne:{
+      width:'852px',
+      height:'310px',
+      objectFit:'cover',
+      margin:'10px'
+    },
+    cardTwo:{
+      width:'467px',
+      height:'310px',
+      margin:'10px'
+    },
+    chartBody:{
+      display:'flex'
+    },
+    Chart:{
+     margin:'auto',
+     width:'700px',
+     height:'250px'
+    }
+  }
+
+const WidgetDropdownChart = () => {
+  return (
+   <div style={style.chartBody}>
+   <CCard style={style.cardOne}>
+   <CCardTitle style={{padding:'10px'}}>Total Vehicles</CCardTitle>
+    <CCardBody>
+    <CChartLine style={style.Chart}
+  type="line" 
+  data={{
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: "rgba(220, 220, 220, 0.2)",
+        borderColor: "rgba(220, 220, 220, 1)",
+        pointBackgroundColor: "rgba(220, 220, 220, 1)",
+        pointBorderColor: "#fff",
+        data: [40, 20, 12, 39, 10, 40, 39, 80, 40]
+      },
+      {
+        label: "My Second dataset",
+        backgroundColor: "rgba(151, 187, 205, 0.2)",
+        borderColor: "rgba(151, 187, 205, 1)",
+        pointBackgroundColor: "rgba(151, 187, 205, 1)",
+        pointBorderColor: "#fff",
+        data: [50, 12, 28, 29, 7, 25, 12, 70, 60]
+      },
+    ],
+  }}
+/>
+
+</CCardBody>
+</CCard>
+
+<CCard style={style.cardTwo}>
+<CCardTitle style={{padding:'10px'}}> Maintenance</CCardTitle>
+  <CCardBody>
+  <CTable>
+ 
+  <CTableBody style={{padding:'50px 0px'}}>
+    <CTableRow>
+      <CTableDataCell>In-Service</CTableDataCell>
+      <CTableDataCell><CBadge color="success" shape="rounded-pill">0</CBadge></CTableDataCell>
+    </CTableRow>
+    <CTableRow>
+      <CTableDataCell>Out-of-Service</CTableDataCell>
+      <CTableDataCell><CBadge color="danger" shape="rounded-pill">0</CBadge></CTableDataCell>
+    </CTableRow>
+    <CTableRow>
+      <CTableDataCell>Disposed</CTableDataCell>
+      <CTableDataCell><CBadge color="dark" shape="rounded-pill">0</CBadge></CTableDataCell>
+    </CTableRow>
+    <CTableRow>
+      <CTableDataCell>Missing</CTableDataCell>
+      <CTableDataCell><CBadge color="warning" shape="rounded-pill">0</CBadge></CTableDataCell>
+    </CTableRow>
+  </CTableBody>
+</CTable>
+  </CCardBody>
+</CCard>
+   </div>
+  )
+}
+
+export default WidgetDropdownChart
