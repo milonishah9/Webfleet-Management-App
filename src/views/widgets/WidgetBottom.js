c8ff5288d6c49a8915117e3d114ed5415dab7a35
syndelldev <106510484+syndelldev@users.noreply.github.com>
2023-02-09 10:01:02 +0530

    Update WidgetBottom.js

--- src/views/widgets/WidgetBottom.js
+++ src/views/widgets/WidgetBottom.js
@@ -83,4 +83,83 @@ const WidgetBottom = () => {
   )
 }
 
-export default WidgetBottom
+ export default WidgetBottom
+
+
+
+// /* eslint-disable */
+// import { CCardHeader,CNavbar, CBadge, CDropdownToggle, CDropdownDivider, CButton, CDropdown, CNavbarToggler, CContainer,  CFormInput, CForm, CDropdownMenu, CNavItem, CNavLink, CNavbarNav, CNavbarBrand, CDropdownItem,  CCollapse,  CCardBody, CCardTitle, CCardText, CCard, CRow, CCol, CCardFooter } from '@coreui/react'
+// import CIcon from '@coreui/icons-react'
+// import React from 'react'
+// import {cilTruck, cilLocationPin} from '@coreui/icons'
+
+// const VehicleTracking = () => {
+   
+//    return (
+//    <div className='tarcking_div'>
+//         <div className='tracking_vehicle'>
+//             <div>
+//             <CFormInput
+//                 type="text"
+//                 placeholder="Search..."
+//                 className='input_vehicle'
+//                 readOnly
+//                 />
+//             </div>
+//             {[
+//             { color: 'dark', textColor: 'black', header: 'Arun', location:'Goa', ship:13874346 ,clientName: 'sharans' },
+//             { color: 'white', textColor: 'black',header: 'Suraj', location:'Nikol', ship:13874347 ,clientName: 'Rohit' },
+//             { color: 'white', textColor: 'black ',header: 'Vikash', location:'Vadodara', ship:13874348 ,clientName: 'Ayushman' },
+//             { color: 'dark', textColor: 'black',header: 'Yogesh' , location:'Surat', ship:13874349,clientName: 'Saiz'},
+//             { color: 'dark' ,header: 'Govind', location:'ahmedabad', ship:13874350,clientName: 'Akash'},
+//             { color: 'dark', textColor: 'Navin',header: 'Hemal', location:'Indore', ship:13874351 ,clientName: 'Neha'},
+//             { color: 'dark',header: 'Jigar', location:'Varanasi', ship:13874352,clientName: 'sharans'},
+//             { color: 'dark', textColor: 'white' ,header: 'Rahul', location:'Pune', ship:13874353,clientName: 'sharans'},
+//             ].map((item, index) => (
+            
+//             <CCard
+//             color={item.color}
+//             textColor='black'
+//             className="mb-3 tracking_card"
+//             style={{ maxWidth: '18rem',margin:'20px' }}
+//             key={index}
+//             >
+//             <CCardHeader><span className='opcity_tracking'>Shipment number</span> <br/>
+//                         <span className='ship_tracking'>{item.ship}</span>  <br/>
+//                         <span className='location_tracking'>{item.location}</span></CCardHeader>
+            
+//             <CCardBody>
+//                 <CCardText>
+//                     <div>
+//                     <CIcon className='icon_tracking' icon={cilLocationPin}/> &nbsp;&nbsp;<span className='place_tracking'>Nikol</span>  <br/>&nbsp;&nbsp;<span className='opcity_tracking'>Manohar villa</span>
+//                     </div>
+//                     <div>
+//                     <CIcon className='icon_tracking' icon={cilTruck} /> &nbsp;&nbsp; <span className='place_tracking'>Naroda</span> <br/>&nbsp;&nbsp;<span className='opcity_tracking'>Parshwanath</span>
+//                     </div>
+               
+               
+//                 </CCardText>
+//             </CCardBody>
+
+//             <CCardFooter>
+//                 <CCardText>
+//                     <span className='opcity_tracking'>Client </span> <br/>
+//                     <span className='ship_tracking'>{item.clientName}</span> <br/>
+//                     <span className='location_tracking'>London,UK</span>
+//                 </CCardText>
+//             </CCardFooter>
+//             </CCard>
+//             ))}
+//         </div>
+//         <div className='tracking_map'>
+
+//         </div>
+
+
+
+// </div>
+  
+//   )
+// }
+
+// export default VehicleTracking
